847b8fad69f4c1ac8c6c70b08a0d2b89590d5290
rushyab <rushyab@gmail.com>
2019-08-16 18:05:44 +0530

    dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-//creating routes
-router.get('/',(req,res)=>{
-    res.render("welcome");
+const { ensureAuthenticated } = require('../config/auth');
 
-})
+// Welcome Page
+router.get('/', (req, res) => res.render('welcome.ejs'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    name: req.user.name
+  })
+);
+
 module.exports = router;
