a1dca6d4e020ae083b02cea312c28209c535a954
rushyab <rushyab@gmail.com>
2019-08-18 20:39:46 +0530

    encryption key added

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
-dbPassword = `mongodb+srv://rushyab1:mypass12345@cluster0-qk06w.mongodb.net/test?retryWrites=true&w=majority`;
+dbPassword = `mongodb+srv://rushyab1:mypass12345@cluster0-qk06w.mongodb.net/pass-manager?retryWrites=true&w=majority`;
+encryptionKey = "F2b5FdGcZ6MIvbBW3WD6h7UL08YUmC4E";
 
 module.exports = {
-  mongoURI: dbPassword
+  mongoURI: dbPassword,
+  encryptionKey
 };
